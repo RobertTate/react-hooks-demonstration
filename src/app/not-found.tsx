45cbd97cebf97b4cdcfc4b14322d85db4b64e9b3
Robert Tate <robert.tate@asurion.com>
2023-10-24 16:56:00 -0500

    fix linting issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <main>
-      <h1>Uh oh! There's nothing here.</h1>
+      <h1>Uh oh! There&apos;s nothing here.</h1>
       <Link href={'/'}>Back to home</Link>
     </main>
   )
